b14b6b4b3ed2baa2696f8d8720e319570cc0fb23
Andrea Fiore <andrea.fiore@composable.io>
2014-05-11 17:19:54 +0100

    Support Java programs (i.e. Intellij)

--- targets/.xmonad/xmonad.hs
+++ targets/.xmonad/xmonad.hs
@@ -1,8 +1,10 @@
 import XMonad
 import XMonad.Config.Gnome
+import XMonad.Hooks.SetWMName
 
 main :: IO ()
 main = xmonad $ gnomeConfig
   { focusFollowsMouse  = True
   , focusedBorderColor = "#D7FF00"
+  , startupHook        = setWMName "LG3D"
   , modMask            = mod4Mask }
